680ae9ca1ea29828e0b526bf731bcd9d225af601
kalmutair <khwlahjp@yahoo.com>
2018-10-25 12:34:54 -0400

    Revert "Assignment Solved"
    This reverts commit dbfc58d05066e3a13ae823fe1daa8db336cc037e.

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,6 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL #m is set to NULL because it going to be filled  by later code in the function.
-        set <- function(y) { # makeCacheMatrix defines the set()function
-                x <<- y # double arrows assigns the value of y to an object in the parent enviroment.
-                m <<- NULL # this is to clear any previous executuin of cacheSolve().
-        }
-        get <- function() x
-        setinverse <- function(inverse) m <<- inverse
-        getinverse <- function() m
-       
-         list(set = set, 
-             get = get,
-             setinverse = setinverse,
-             getinverse = getinverse)
-#here each element in the list should be listed and this allow to access the functions by name using $ sign.
 
 }
 
@@ -26,15 +12,4 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-     
-                m <- x$getinverse()  #the function get the inverse from the object passed in as the argument
-                if(!is.null(m)) {  #check wheather the result is NULL.
-                        message("getting cached data")
-                        return(m)
-                }
-                data <- x$get()
-                m <- solve(data, ...)
-                x$setinverse(m)
-                m
-      
 }
